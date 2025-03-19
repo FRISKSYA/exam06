320e7e010c2145a44ca299df38fb782ec2187401
kfukuhar <kfukuhar@student.42tokyo.jp>
2025-03-19 20:32:19 +0900

    init

---
+++ mini_serv.c
@@ -0,0 +1,66 @@
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int extract_message(char **buf, char **msg)
+{
+	char	*newbuf;
+	int		i;
+
+	*msg = 0;
+	if (*buf == 0)
+		return (0);
+	i = 0;
+	while ((*buf)[i])
+	{
+		if ((*buf)[i] == '\n')
+		{
+			newbuf = malloc(sizeof(*newbuf) * (strlen(*buf + i + 1) + 1));
+			if (newbuf == NULL)
+				return (-1);
+			strcpy(newbuf, *buf + i + 1);
+			*msg = *buf;
+			(*msg)[i + 1] = 0;
+			*buf = newbuf;
+			return (1);
+		}
+		++i;
+	}
+	return (0);
+}
+
+char *str_join(char *buf, char *add)
+{
+	char	*newbuf;
+	int		len;
+
+	if (buf == 0)
+		len = 0;
+	else
+		len = strlen(buf);
+	newbuf = malloc(sizeof(*newbuf) * (len + strlen(add) + 1));
+	if (newbuf == NULL)
+		return (0);
+	if (buf != 0)
+		strcat(newbuf, buf);
+	free(buf);
+	strcat(newbuf, add);
+	return (newbuf);
+}
+
+int main(int argc, char **argv)
+{
+	int sockfd, connfd, max_fd, ret;
+	socklen_t	len;
+	struct timeval timeout;
+	struct sockaddr_in servaddr, cli;
+	fd_set set_read, set_write;
+	char buff[4096];
+	ssize_t ssize_len;
+
+	return 0;
+}
