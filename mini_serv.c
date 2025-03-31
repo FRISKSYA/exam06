fbd3c64c00e6ee48268470b23040c3e86093d3a0
kfukuhar <kfukuhar@student.42tokyo.jp>
2025-03-31 15:07:32 +0900

    wip

--- mini_serv.c
+++ mini_serv.c
@@ -4,17 +4,17 @@
 #include <netdb.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct s_client{
-	int	id;
-	char *msg;
-}	t_client;
+typedef struct s_client {
+    int id;
+    char *msg;
+}   t_client;
 
-t_client clients[FD_SETSIZE];
-fd_set master_set, read_set, write_set;
-int	next_id = 0, max_fd = 0;
+t_client    clients[FD_SETSIZE];
+fd_set  master_set, read_set, write_set;
+int max_fd = 0, next_id = 0;
 
 int extract_message(char **buf, char **msg)
 {
@@ -63,16 +63,72 @@ char *str_join(char *buf, char *add)
 	return (newbuf);
 }
 
-void	fatal_error(){
-	write(2, "Fatal error\n", 12);
-	exit(1);
+void    fatal_error(){
+    write(2, "Fatal error\n", 12);
+    exit(1);
+}
+
+void    send_all(int except_fd, char *msg) {
+    for (int fd = 0; fd <= max_fd; ++fd) {
+        if (FD_ISSET(fd, &write_set) && fd != except_fd) {
+            if (send(fd, msg, strlen(msg), 0) < 0)
+                fatal_error();
+        }
+    }
+}
+
+void    handle_new_connection(int server_fd) {
+    struct sockaddr_in client_addr;
+    socklen_t addr_len = sizeof(client_addr);
+
+    int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len);
+    if (client_fd < 0)
+        fatal_error();
+    FD_SET(client_fd, &master_set);
+    if (client_fd > max_fd)
+        max_fd = client_fd;
+    
+        clients[client_fd].id = next_id++;
+        clients[client_fd].msg = NULL;
+
+        char    buffer[100000];
+        sprintf(buffer, "server: client %d just arrived\n", clients[client_fd].id);
+        send_all(client_fd, buffer);
+}
+
+void    handle_client_message(int client_fd) {
+    char    recv_buf[100000];
+    int     bytes = recv(client_fd, recv_buf, sizeof(recv_buf) - 1, 0);
+
+    if (bytes <= 0) {
+        char    buffer[100000];
+        sprintf(buffer, "server: client %d just left\n", clients[client_fd].id);
+        send_all(client_fd, buffer);
+
+        FD_CLR(client_fd, &master_set);
+        close(client_fd);
+        if (clients[client_fd].msg)
+            free(clients[client_fd].msg);
+        clients[client_fd].msg = NULL;
+    } else {
+        recv_buf[bytes] = '\0';
+        clients[client_fd].msg = str_join(clients[client_fd].msg, recv_buf);
+
+        char    *msg = NULL;
+        while (extract_message(&clients[client_fd].msg, &msg) == 1) {
+            char    buffer[100000];
+            sprintf(buffer, "client %d: %s", clients[client_fd].id, msg);
+            send_all(client_fd, buffer);
+            free(msg);
+        }
+    }
 }
 
 int main(int argc, char **argv) {
-	if (argv != 2) {
-		write(2, "Wrong number of arguments\n", 26);
-		exit(1);
-	}
+    if (argc != 2) {
+        write(2, "Wrong number of arguments\n", 26);
+        exit(1);
+    }
 
 	int sockfd, connfd, len;
 	struct sockaddr_in servaddr, cli; 
@@ -80,8 +136,8 @@ int main(int argc, char **argv) {
 	// socket create and verification 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
 	if (sockfd == -1)
-		fatal_error();
-	bzero(&servaddr, sizeof(servaddr)); 
+        fatal_error();
+    bzero(&servaddr, sizeof(servaddr)); 
 
 	// assign IP, PORT 
 	servaddr.sin_family = AF_INET; 
@@ -90,17 +146,31 @@ int main(int argc, char **argv) {
   
 	// Binding newly created socket to given IP and verification 
 	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
-		fatal_error();
+        fatal_error();
 	if (listen(sockfd, SOMAXCONN) != 0)
-		fatal_error();
-	// len = sizeof(cli);
-	// connfd = accept(sockfd, (struct sockaddr *)&cli, &len);
-	// if (connfd < 0)
-	// 	fatal_error();
+        fatal_error();
 
-	FD_ZERO(&master_set);
-	FD_SET(sockfd, &master_set);
-	max_fd = sockfd;
+    FD_ZERO(&master_set);
+    FD_SET(sockfd, &master_set);
+    max_fd = sockfd;
 
-	for (int )
+    for (int i = 0; i < FD_SETSIZE; ++i)
+        clients[i].msg = NULL;
+
+    while (1){
+        read_set = write_set = master_set;
+
+        if (select(max_fd + 1, &read_set, &write_set, NULL, NULL) < 0)
+            fatal_error();
+
+        for (int fd = 0; fd <= max_fd; ++fd) {
+            if (FD_ISSET(fd, &read_set)){
+                if (fd == sockfd)
+                    handle_new_connection(sockfd);
+                else
+                    handle_client_message(fd);
+            }
+        }
+    }
+    return (0);
 }
